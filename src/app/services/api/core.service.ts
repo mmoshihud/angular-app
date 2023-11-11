29b98480aceaa96f94e0685fd88f49d95b3428cd
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-11-11 14:32:26 +0600

    Created a get all products for Http Request

--- src/app/services/api/core.service.ts
+++ src/app/services/api/core.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoreService {
+  private apiUrl = 'https://fakestoreapi.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProduct(): Observable<Object> {
+    const productsUrl: string = `${this.apiUrl}/products`;
+    return this.http.get(productsUrl);
+  }
 }
